e4e5cfaa9a8da886f0b567ca88e0244d789ed3c3
AnkaMikola <ankoowl@gmail.com>
2024-10-21 16:42:17 +0300

    homework #3

--- Homeworks/homework_03.py
+++ Homeworks/homework_03.py
@@ -6,8 +6,8 @@ alice_in_wonderland = ('''"Would you tell me, please, which way'''
                        '''\n"I don't much care where —— " said Alice.\n'''
                        '''"Then it doesn't matter which way'''
                        '''you go," said the Cat.\n"'''
-                       '''—— so long as I get somewhere,"'''
-                       '''Alice added as an explanation.\n'''
+                       ''' —— so long as I get somewhere,"'''
+                       ''' Alice added as an explanation.\n'''
                        '''"Oh, you're sure to do that," said the'''
                        '''Cat, "if you only walk long enough."''')
 print(alice_in_wonderland)
