ee4083cf7cb54f7f7a7830d1dc555a905319a0a9
AnkaMikola <ankoowl@gmail.com>
2024-11-14 14:53:06 +0200

    homework 06.1.виправлене

--- Homeworks/homework_06.1.py
+++ Homeworks/homework_06.1.py
@@ -1,10 +1,13 @@
 """here is docstring."""
 
 # Порахувати кількість унікальних символів в строці.
-# Якщо їх більше 10 - вивести в консоль True, інакше - False.
+# Якщо їх більше 10, то вивести в консоль True, інакше - False.
 # Строку отримати за допомогою функції input()
 
 some_symbols = input('Let your cat walk on your keyboard. Than press "Enter" ')
 
 unique_sym_count = len(set(some_symbols))
-print(f'Unique symbols amount: {unique_sym_count}.')
+if unique_sym_count > 10:
+    print(True)
+else:
+    print(False)
